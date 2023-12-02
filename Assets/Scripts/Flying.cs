9e2db9c2d08574ea4239bcfde4a3dfee922c2f3a
ttzhang0 <152727258+ttzhang0@users.noreply.github.com>
2023-12-02 13:42:14 -0800

    Bird Script

--- Assets/Scripts/Flying.cs
+++ Assets/Scripts/Flying.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Flying : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 direction;
+    
+    public float gravity = -10f;
+    public float strength = 5f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)){
+            direction = Vector3.up*strength;
+
+        }
+
+        if (Input.touchCount > 0){
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began){
+                direction = Vector3.up * strength;
+            }
+        }
+
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
     }
 }
